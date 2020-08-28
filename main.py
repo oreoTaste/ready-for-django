88b0514cadabf89f9574ae30d447e19b93087ea0
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-28 16:24:33 +0900

    3.5 Extending Classes

--- main.py
+++ main.py
@@ -7,20 +7,38 @@ class Car:
         self.color = kwargs.get("color", "nocolor")
         self.name = kwargs.get("name", "noname")
 
+    def printName(self):
+        print(self.name)
+
     def __str__(self):
         try:
             return (
-                f"{self.color} {self.name} car with {self.wheel} wheels, {self.door} door, {self.window} window, {self.seat} seat \n"
-                + super().__str__()
+                f"{self.color} {self.name} car with {self.wheel} wheels, {self.door} door, {self.window} window, {self.seat} seat"
+                # + super().__str__()
             )
         except AttributeError:
             return f"car with {self.wheel} wheels + " + super.__str__(self)
 
 
+class Convertible(Car):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.type = kwargs.get("type")
+
+    def printName(self):
+        print("Convertible...", end=" ")
+        super().printName()
+
+    def __str__(self):
+        return self.type + "... " + super().__str__()
+
+
 hyundai = Car(color="green", name="hyundai")
+hyundai.printName()
 print(hyundai, "\n")
 
-noname = Car()
+noname = Convertible(color="yellow", name="noname", type="convertible")
+noname.printName()
 print(noname)
 
 
