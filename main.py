7e8ab63b36d2af93a3e2fbff8024fc2a25c96165
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-28 14:58:09 +0900

    3.2 Intro to Object Oriented Programming

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+class Car:  # class : blueprint of instance
+    wheel = 4
+    door = 4
+    window = 4
+    seat = 4
+
+
+porsche = Car()  # instance : the real object of class
+porsche.name = "porsche"
+hyundai = Car()
+hyundai.name = "hyundai"
+
+print(porsche, porsche.door, porsche.name)
+print(hyundai, hyundai.door, hyundai.name)
+
+
 def add(*args, **kwargs):
 
     print(type(args), args)  # tuple <- positional argument
@@ -9,6 +25,3 @@ def add(*args, **kwargs):
     for num in args:
         result += num
     print(f"--> result : {result}")
-
-
-add(1, 2, 3, 4, 5, name="Mike")
